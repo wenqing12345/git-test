a149a1b816284d6c0896922775b2f21f2d6c9b6f
wenqing <3139835023@qq.com>
2023-05-30 14:23:24 +0800

    third commit

--- src/main/java/com/hncu/git/GitTest.java
+++ src/main/java/com/hncu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git2!");
+        System.out.println("hello git3!");
     }
 }
