258d13307f4f23375cd08b5249ba3e74c70919d9
wenqing <3139835023@qq.com>
2023-05-30 14:22:09 +0800

    second commit

--- src/main/java/com/hncu/git/GitTest.java
+++ src/main/java/com/hncu/git/GitTest.java
@@ -9,5 +9,6 @@ package com.hncu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
