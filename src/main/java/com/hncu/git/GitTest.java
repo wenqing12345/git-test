f21fb9cbac4c3d7229816528aae3607e9a3953ff
wenqing <3139835023@qq.com>
2023-05-30 14:17:37 +0800

    first commit

---
+++ src/main/java/com/hncu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.hncu.git;
+
+/**
+ * @author wenqing
+ * @version 1.0
+ * @date 2023/5/30 14:10
+ * @mood happy
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
